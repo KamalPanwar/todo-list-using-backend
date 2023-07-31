d26c26a4cc1737d1fbd30d5e3d0492b6223e258d
Kamal Panwar <you@example.com>
2023-08-01 01:14:17 +0530

    added post request

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+var todayTaskArr=[]
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
     res.render('index.ejs')
+})
+
+app.post('/',(req,res)=>{
+   todayTaskArr.push (req.body.Task);
+   console.log(todayTaskArr);
+   res.render('index.ejs',{
+    arr:todayTaskArr
+   })
+   
 })
 
 app.listen(port, () => {
